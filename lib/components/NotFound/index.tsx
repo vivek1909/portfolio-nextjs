2e583af533af4d2f1dc820a8cd63a2769352b73f
Vivek Mittal <vivekmittal@affordmed.com>
2022-03-16 22:27:25 +0530

    feat: added custom font

--- lib/components/NotFound/index.tsx
+++ lib/components/NotFound/index.tsx
@@ -2,10 +2,49 @@ import { FC } from "react";
 import Image from "next/image";
 import classes from "./NotFound.module.css";
 import Link from "next/link";
+import Head from "next/head";
 
 export const NotFound: FC = () => {
   return (
     <div className={classes.root}>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-Italic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-SemiBold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-SemiBoldItalic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-Bold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-BoldItalic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <h2>Oops! Page not found.</h2>
       <p>You seem lost! Maybe go to homepage and search again.</p>
       <Image
--- lib/layouts/BaseLayout/index.tsx
+++ lib/layouts/BaseLayout/index.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { FC } from "react";
 import { MenuItems } from "../../../types/menu";
 import { Footer } from "../../components/Footer";
@@ -11,6 +12,44 @@ interface Props {
 export const BaseLayout: FC<Props> = ({ children, activeMenu }) => {
   return (
     <div className={classes.root}>
+      <Head>
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-Regular.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-Italic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-SemiBold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-SemiBoldItalic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-Bold.ttf"
+          as="font"
+          crossOrigin=""
+        />
+        <link
+          rel="preload"
+          href="/fonts/Montserrat/Montserrat-BoldItalic.ttf"
+          as="font"
+          crossOrigin=""
+        />
+      </Head>
       <Navbar activeMenu={activeMenu} />
       <div className={classes.mainContent}>{children}</div>
       <Footer />
