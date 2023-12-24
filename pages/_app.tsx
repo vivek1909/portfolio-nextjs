e178ecd250096314fb2a3a1c2c06fa23e158ad8e
Vivek Mittal <vivekmittal199@gmail.com>
2023-12-24 11:04:26 +0530

    adds new analytics package from vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,14 @@
 import type { AppProps } from "next/app";
 import "react-toastify/dist/ReactToastify.css";
 import "tailwindcss/tailwind.css";
-import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
+      <SpeedInsights />
     </>
   )
 }
